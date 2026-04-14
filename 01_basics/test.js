c0f3fcbe9ecacfe20ab0054bec34cc8304f4632a
Rakshee Tripathy <raksheetripathy6@gmail.com>
2026-04-14 21:39:00 +0530

    my 1st commit

---
+++ 01_basics/test.js
@@ -0,0 +1,9 @@
+console.log("Rakshee")
+const accntId=1245
+let name="Rakshee"
+var acntpswd="8908"
+acntCity="Bangalore"
+let acntState
+
+console.log(accntId)
+console.table([accntId,name,acntpswd,acntCity,acntState])
---
+++ 02_datatypes/datatypes1.js
@@ -0,0 +1,10 @@
+let name="kunu"
+let age=28
+let isLoggedin=false 
+
+
+//nulll- standalove value
+//undefined- variable is declared but not assigned any value
+
+
+console.log(typeof "name") //string
