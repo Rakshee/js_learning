df8fa31221128175a91ab9fa186a667a2fc2edc9
Rakshee Tripathy <raksheetripathy6@gmail.com>
2026-04-16 12:39:11 +0530

    changes 1

---
+++ 01_basics/02_datatypes1.js
@@ -0,0 +1,29 @@
+let name="kunu"
+let age=28
+let isLoggedin=false 
+
+
+//nulll- standalove value
+//undefined- variable is declared but not assigned any value
+
+
+console.log(typeof "name") //string
+
+//Summary
+
+const heros=["aa","nn","ssg"];
+
+
+//pbject
+let myObj={
+    name:"kunu",
+    age:28,
+    isLoggedin:false,
+}
+
+const myFunction=function(){
+    console.log("hello world")
+}
+
+
+console.log(typeof myFunction)
---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,30 @@
+// let score="55"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber=Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber)
+
+
+// "33" =>33
+//"33abc" =>NaN
+//true =>1
+//false =>0
+
+console.log(2+2)
+
+let str1="hi"
+let str2="hello"
+
+let str3=str1+str2
+console.log(str3)
+
+console.log(str1+str2)
+
+
+console.log("1"+2+3)
+console.log(1+"2")
+console.log(1+2+"3")
